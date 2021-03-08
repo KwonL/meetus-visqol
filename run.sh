92e3780d3105b9b5a5704cfce13edae22d6d58fe
KwonL <lkh116@snu.ac.kr>
2021-03-09 08:54:09 +0900

    Remove redundant file

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -it -v /Users/kwonl/projects/skt/meetus-audio:/data jonashaag/visqol:v3 \
-  --degraded_file /data/padding.wav \
-  --reference_file /data/havard.wav \
-  --verbose
